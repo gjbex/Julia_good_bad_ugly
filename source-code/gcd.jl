522c4b11e48903e4fc65fb6a4aeac22a558d86be
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-06-28 10:46:30 +0200

    Use ArgParse to handle command line arguments

--- source-code/gcd.jl
+++ source-code/gcd.jl
@@ -1,11 +1,23 @@
 #!/usr/bin/env julia
 
+using ArgParse
+
 function main()
-    if length(ARGS) != 2
-        error("two arguments expected")
+    arg_parser = ArgParseSettings()
+    arg_parser.description = "Compute the greatest common divisor of two integers"
+    @add_arg_table arg_parser begin
+        "a"
+            arg_type = Int
+            required = true
+            help = "first integer"
+        "b"
+            arg_type = Int
+            required = true
+            help = "second integer"
     end
-    a = parse(Int, ARGS[1])
-    b = parse(Int, ARGS[2])
+    options = parse_args(ARGS, arg_parser)
+    a = options["a"]
+    b = options["b"]
 
     while a != b
         if a > b
