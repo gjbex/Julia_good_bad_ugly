ec13359bf7310c2a04560aad82c4841f36a8040d
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-07-06 15:26:19 +0200

    Apply Julia function naming convention

--- source-code/types/Points.jl
+++ source-code/types/Points.jl
@@ -1,6 +1,8 @@
 module Points
 
-export PointStyle, Is2DPoint, Is3DPoint, get_x, get_y, get_z, distance
+export PointStyle, Is2DPoint, Is3DPoint
+export get_x, get_y, get_z, set_x!, set_y!, set_z!
+export distance
 
 abstract type PointStyle end
 struct Is2DPoint <: PointStyle end
@@ -10,32 +12,31 @@ struct IsNotPoint <: PointStyle end
 PointStyle(::Type) = IsNotPoint()
 
 # x accessors
-"get x-coordinate of a type with the PointStyle trait"
 get_x(p::T) where {T} = get_x(PointStyle(T), p)
 get_x(::Union{Is2DPoint, Is3DPoint}, p) = p.x
 get_x(::IsNotPoint, p) = throw(MethodError(get_x, p))
 
-set_x(p::T, val::Real) where {T} = set_x(PointStyle(T), p, val)
-set_x(::Union{Is2DPoint, Is3DPoint}, p, val) = begin p.x = val; end
-set_x(::IsNotPoint, p, val) = throw(MethodError(set_x, [p, val]))
+set_x!(p::T, val::Real) where {T} = set_x!(PointStyle(T), p, val)
+set_x!(::Union{Is2DPoint, Is3DPoint}, p, val) = begin p.x = val; end
+set_x!(::IsNotPoint, p, val) = throw(MethodError(set_x, [p, val]))
 
 # y accessors
 get_y(p::T) where {T} = get_y(PointStyle(T), p)
 get_y(::Union{Is2DPoint, Is3DPoint}, p) = p.y
 get_y(::IsNotPoint, p) = throw(MethodError(get_y, p))
 
-set_y(p::T, val::Real) where {T} = set_y(PointStyle(T), p, val)
-set_y(::Union{Is2DPoint, Is3DPoint}, p, val) = begin p.y = val; end
-set_y(::IsNotPoint, p, val) = throw(MethodError(set_y, [p, val]))
+set_y!(p::T, val::Real) where {T} = set_y!(PointStyle(T), p, val)
+set_y!(::Union{Is2DPoint, Is3DPoint}, p, val) = begin p.y = val; end
+set_y!(::IsNotPoint, p, val) = throw(MethodError(set_y, [p, val]))
 
 # z accessors
 get_z(p::T) where {T} = get_z(PointStyle(T), p)
 get_z(::Is3DPoint, p) = p.z
 get_z(::IsNotPoint, p) = throw(MethodError(get_z, p))
 
-set_z(p::T, val::Real) where {T} = set_z(PointStyle(T), p, val)
-set_z(::Is3DPoint, p, val) = begin p.z = val; end
-set_z(::IsNotPoint, p, val) = throw(MethodError(set_z, [p, val]))
+set_z!(p::T, val::Real) where {T} = set_z!(PointStyle(T), p, val)
+set_z!(::Is3DPoint, p, val) = begin p.z = val; end
+set_z!(::IsNotPoint, p, val) = throw(MethodError(set_z, [p, val]))
 
 # distance function
 distance(p1::T, p2::T) where {T} = distance(PointStyle(T), p1, p2)
